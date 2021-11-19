b1023d70331e8540eeafa6173531a9bc57ea0358
maxfer1221 <maxfer12221@gmail.com>
2021-11-19 01:03:11 -0500

    4x speed increase and quality options

--- src/main.rs
+++ src/main.rs
@@ -4,40 +4,40 @@ use std::{time::Instant, sync::{Arc, Mutex, atomic::{Ordering::SeqCst, AtomicU8}
 use std::thread;
 use crossbeam::thread as cb_thread;
 
+//split into arrays % 3
 struct FeatureField {
-    field: Vec<Vec<AtomicU8>>
+    field: Vec<Vec<AtomicU8>>,
+    center: (u8, u8),
 }
 
 trait Helper where Self: Sized {
-    fn feature_image(&self, dim: (u32, u32), ff: &FeatureField, th: u8) -> Result<(), ()>; 
+    fn feature_image(&self, dim: (u32, u32), ff: &FeatureField, th: u8, step: usize) -> Result<(), ()>; 
     fn bounding_box(self, ff: &FeatureField) -> Self;
 }
 impl Helper for RgbImage {
-    fn feature_image(&self, dim: (u32, u32), ff: &FeatureField, th: u8) -> Result<(), ()> {
+    fn feature_image(&self, dim: (u32, u32), ff: &FeatureField, th: u8, step: usize) -> Result<(), ()> {
         let mut img_mut = Arc::new(Mutex::new(RgbImage::new(dim.0, dim.1)));
         let ud = (dim.0 as usize, dim.1 as usize);
-        let coords: Arc<Mutex<(u32, u32)>> = Arc::new(Mutex::new((3, 3)));
 
+        let counter: AtomicU8 = AtomicU8::new(1);
 
+        let now = Instant::now();
         cb_thread::scope(|s| {
             let mut threads = Vec::new();
             for i in 0..th {
-                threads.push(s.spawn(|_| loop {
-                    let mut c = coords.lock().unwrap();
-                    let ct = c.clone();
-                    if c.0 < dim.0 - 5 {
-                        c.0 += 2;
-                    } else if c.1 < dim.1 - 5 {
-                        c.1 += 2;
-                        c.0 = 3;
-                    } else {
-                        break;
-                    }
-                    drop(c);
-                    // let x = ff.field[ct.0 as usize][ct.1 as usize].lock().unwrap();
-                    if ff.field[ct.0 as usize][ct.1 as usize].load(SeqCst) == 1 {
-                        let mut img_mut = img_mut.lock().unwrap();
-                        (*img_mut).put_pixel(ct.0, ct.1, Rgb([200; 3]));
+                threads.push(s.spawn(|_| {
+                    let c = counter.fetch_add(1, SeqCst);
+                    let bound: usize =
+                        (((ud.1 as f64 - 4.0) / th as f64) / step as f64).floor() as usize;
+                    println!("{}", bound);
+                    for x in (3..(ud.0 - 3)).step_by(step) {
+                        for yt in (2..(bound)) {
+                            let y = (yt * step * th as usize) + (step * c as usize);
+                            if ff.field[x][y].load(SeqCst) == 1 {
+                                let mut img_mut = img_mut.lock().unwrap();
+                                (*img_mut).put_pixel(x as u32, y as u32, Rgb([255; 3]));
+                            }
+                        }
                     }
                 }));
             }
@@ -45,16 +45,14 @@ impl Helper for RgbImage {
                 thread.join().unwrap();
             }
         }).unwrap();
-        let t = Instant::now();
-        println!("{:?}", t.elapsed());
+        println!("{:?}", now.elapsed());
+
         let mut img_mut = img_mut.lock().unwrap();
         (*img_mut).save("cross.png").unwrap();
         Ok(())
     }
 
     fn bounding_box(self, ff: &FeatureField) -> Self { 
-        // concentrate in average location of all points
-        // increase box size until efficiency loss
         self
     }
 }
@@ -65,6 +63,7 @@ fn main() {
     let img_name: &String = &args[1];
     let t: i16 = args[2].parse::<i16>().unwrap();
     let th: u8 = args[3].parse::<u8>().unwrap();
+    let step: usize = args[4].parse::<usize>().unwrap();
 
     let (mut img, dimensions) = match open(img_name) {
         Err(e) => {
@@ -74,12 +73,10 @@ fn main() {
         Ok(i) => (i.into_rgb8(), image::image_dimensions(img_name).unwrap())
     };
 
-    // let mut feature_field: RgbImage = detect_features(&img, dimensions, t).unwrap();
-    let feature_field = detect_features_clean(&img, dimensions, t, th).unwrap();
+    let feature_field = detect_features_clean(&img, dimensions, t, th, step).unwrap();
     img = img.bounding_box(&feature_field);
-    let i = img.feature_image(dimensions, &feature_field, th);
+    let i = img.feature_image(dimensions, &feature_field, th, step);
     create_final_image(&img);
-    // let img_pixels: image::Pixels = img.pixels();
 }
 
 fn detect_features_clean(
@@ -87,60 +84,48 @@ fn detect_features_clean(
     dim: (u32, u32),
     t: i16,
     th: u8,
+    step: usize,
     ) -> std::io::Result<FeatureField> {
 
     let ud = (dim.0 as usize, dim.1 as usize);
     let img = imageops::grayscale(img);
 
-    let coords: Arc<Mutex<(u32, u32)>> = Arc::new(Mutex::new((3, 3)));
     let mut fm: Vec<Vec<AtomicU8>> = Vec::with_capacity(ud.0);
-    for y in 0..ud.0 {
-        fm.push(Vec::with_capacity(ud.1));
-        for x in 0..ud.1 {
-            fm[y].push(AtomicU8::new(0));
+    let mut pixels: Vec<Vec<i16>> = Vec::with_capacity(ud.0);
+
+
+    for x in 0..ud.0 {
+        pixels.push(Vec::with_capacity(ud.0));
+        for y in 0..ud.1 {
+            pixels[x].push(img.get_pixel(x as u32, y as u32).channels()[0] as i16);
         }
     }
-        // vec![vec![Arc::new(Mutex::new(0u8)); ud.1]; ud.0];
 
-    // let mut counter = (0u64, 0u64);
-    // for i in &fm {
-    //     for j in  i {
-    //         let v = j.lock().unwrap();
-    //         if *v == 1 {
-    //             counter.0 += 1;
-    //         } else {
-    //             counter.1 += 1;
-    //         }
-    //     }
-    // }
-    // println!("{:?}", counter);
-    let counter = Arc::new(Mutex::new((0u64, 0u64)));
-
+    for x in 0..ud.0 {
+        fm.push(Vec::with_capacity(ud.1));
+        for y in 0..ud.1 {
+            fm[x].push(AtomicU8::new(0));
+        }
+    }
+    
+    let counter: AtomicU8 = AtomicU8::new(1);
+    
     let now = Instant::now();
     cb_thread::scope(|s| {
         let mut threads = Vec::new();
         for i in 0..th {
-            threads.push(s.spawn(|_| loop {
-                let mut c = coords.lock().unwrap();
-                let ct = c.clone();
-                if c.0 < dim.0 - 5 {
-                    c.0 += 2;
-                } else if c.1 < dim.1 - 5 {
-                    c.1 += 2;
-                    c.0 = 3;
-                } else {
-                    break;
-                }
-                drop(c);
-                let p_val = get_feature_value(&img, ct.0, ct.1, t);
-                let mut counter = counter.lock().unwrap();
-                // let mut fm_t = fm[ct.0 as usize][ct.1 as usize].lock().unwrap();
-                // println!("{:?}", fm_t);
-                if p_val == 1 {
-                    fm[ct.0 as usize][ct.1 as usize].store(1, SeqCst);
-                    (*counter).0 += 1
-                } else {
-                    (*counter).1 += 1;
+            threads.push(s.spawn(|_| {
+                let c = counter.fetch_add(1, SeqCst);
+                let bound: usize =
+                    (((ud.1 as f64 - 4.0) / th as f64) / step as f64).floor() as usize;
+                println!("{}", bound);
+                for x in (3..(ud.0 - 3)).step_by(step) {
+                    for yt in (2..(bound)) {
+                        let y = (yt * step * th as usize) + (step * c as usize);
+                        if get_feature_value(&pixels, x, y, t) == 1 {
+                            fm[x][y].store(1, SeqCst);
+                        }
+                    }
                 }
             }));
         }
@@ -148,38 +133,49 @@ fn detect_features_clean(
             thread.join().unwrap();
         }
     }).unwrap();
-
     println!("{:?}", now.elapsed());
-    // let mut val = fm[0][0].lock().unwrap();
-    // *val = 0;
-    // drop(val);
-    // let mut counter = (0u64, 0u64);
-    // for i in &fm {
-    //     for j in  i {
-    //         let v = j.lock().unwrap();
-    //         if *v == 1 {
-    //             counter.0 += 1;
-    //         } else {
-    //             counter.1 += 1;
-    //         }
-    //     }
-    // }
-    // println!("{:?}", counter);
 
-    Ok(FeatureField { field: fm })
+
+    // let now = Instant::now();
+    // cb_thread::scope(|s| {
+    //     let mut threads = Vec::new();
+    //     for i in 0..th {
+    //         threads.push(s.spawn(|_| loop {
+    //             let mut c = coords.lock().unwrap();
+    //             let ct = c.clone();
+    //             if c.0 < dim.0 - 5 {
+    //                 c.0 += 1;
+    //             } else if c.1 < dim.1 - 5 {
+    //                 c.1 += 1;
+    //                 c.0 = 3;
+    //             } else {
+    //                 break;
+    //             }
+    //             drop(c);
+    //             if get_feature_value(&pixels, ct.0 as usize, ct.1 as usize, t) == 1 {
+    //                 fm[ct.0 as usize][ct.1 as usize].store(1, SeqCst);
+    //             }
+    //         }));
+    //     }
+    //     for thread in threads {
+    //         thread.join().unwrap();
+    //     }
+    // }).unwrap();
+    // println!("{:?}", now.elapsed());
+
+    Ok(FeatureField { field: fm, center: (0, 0) })
 }
 
-fn get_feature_value(
-    img: &ImageBuffer<impl Pixel<Subpixel = u8> + std::fmt::Debug + 'static, Vec<u8>>, x: u32, y: u32, t: i16) -> u8 {
-;
+fn get_feature_value(pa: &Vec<Vec<i16>>, x: usize, y: usize, t: i16) -> u8 {
+
     let mut ps = Vec::with_capacity(16);
-    let cp = img.get_pixel(x, y).channels()[0] as i16;
+    let cp = pa[x][y];
 
     ps.append(&mut vec![
-        img.get_pixel(x, y - 3).channels()[0] as i16,
-        img.get_pixel(x + 3, y).channels()[0] as i16,
-        img.get_pixel(x, y + 3).channels()[0] as i16,
-        img.get_pixel(x - 3, y).channels()[0] as i16,
+        pa[x][y - 3],
+        pa[x + 3][y],
+        pa[x][y + 3],
+        pa[x - 3][y],
     ]);
     
     let mut count = 0;
@@ -194,18 +190,18 @@ fn get_feature_value(
     }
 
     ps.append(&mut vec![
-        img.get_pixel(x + 1, y - 3).channels()[0] as i16,
-        img.get_pixel(x + 2, y - 2).channels()[0] as i16,
-        img.get_pixel(x + 3, y - 1).channels()[0] as i16,
-        img.get_pixel(x + 3, y + 1).channels()[0] as i16,
-        img.get_pixel(x + 2, y + 2).channels()[0] as i16,
-        img.get_pixel(x + 1, y + 3).channels()[0] as i16,
-        img.get_pixel(x - 1, y + 3).channels()[0] as i16,
-        img.get_pixel(x - 2, y + 2).channels()[0] as i16,
-        img.get_pixel(x - 3, y + 1).channels()[0] as i16,
-        img.get_pixel(x - 3, y - 1).channels()[0] as i16,
-        img.get_pixel(x - 2, y - 2).channels()[0] as i16,
-        img.get_pixel(x - 1, y - 3).channels()[0] as i16,
+        pa[x + 1][y - 3],
+        pa[x + 2][y - 2],
+        pa[x + 3][y - 1],
+        pa[x + 3][y + 1],
+        pa[x + 2][y + 2],
+        pa[x + 1][y + 3],
+        pa[x - 1][y + 3],
+        pa[x - 2][y + 2],
+        pa[x - 3][y + 1],
+        pa[x - 3][y - 1],
+        pa[x - 2][y - 2],
+        pa[x - 1][y - 3],
     ]);
     
     for pv in ps[4..].iter() {
@@ -217,8 +213,8 @@ fn get_feature_value(
     if count < 12 {
         return 0;
     }
+    
     1
-
 }
 
 
